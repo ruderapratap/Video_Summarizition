c774d4e1e97d5103750a311b11caf9b833d182ae
Rudera Pratap Singh <132190111+ruderapratap@users.noreply.github.com>
2026-06-24 16:55:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from audio_processor import process_input
 from transcriber import transcribe_all
 from summarizer import summarize, generate_title
-from extractor import extract_action_items, extract_key_decisions, extract_questions
+from insight_extractor import extract_action_items, extract_key_decisions, extract_questions
 from rag_engine import build_rag_chain, ask_question
 
 
