cf4a40de6b02ba7986447441fadfd8b7e5a21250
Rudera Pratap Singh <132190111+ruderapratap@users.noreply.github.com>
2026-06-24 16:03:31 +0530

    Update rag_engine.py

--- rag_engine.py
+++ rag_engine.py
@@ -3,7 +3,7 @@ from langchain_mistralai import ChatMistralAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableLambda
-from core.vector_store import build_vector_store, load_vector_store, get_retriever
+from vector_store import build_vector_store, load_vector_store, get_retriever
 
 def get_llm():
     return ChatMistralAI(
